d21817d98dccbe7339e7d34fc226c065fa5dc5fc
morela1 <morela1@pc-dg-027-13>
2017-01-06 13:36:00 +0100

    Merge origin/master
    Conflicts:
    	ileInterdite/src/model/aventuriers/Aventurier.java
    	ileInterdite/src/model/cases/Tuile.java

--- ileInterdite/src/controler/Controleur.java
+++ ileInterdite/src/controler/Controleur.java
@@ -7,6 +7,7 @@ import model.aventuriers.Aventurier;
 import model.cases.Tuile;
 import util.Utils;
 import util.Utils.EtatTuile;
+import static util.Utils.EtatTuile.ASSECHEE;
 import util.Utils.Pion;
 
 /**
@@ -22,33 +23,41 @@ public class Controleur implements Observer {
         
     }
 
-    public void Assecher(Aventurier av, Tuile tuile){
-        Pion capacite;
-        capacite= av.getCapacite();
-        
-//         //if (tuile.getEtat()== EtatTuile.ASSECHEE && tuileAdjacente()){
-//             
-//             
-//         }
-//         
-//        
-        
-         
-         
-        
-        
+    
+       private boolean tuileEstDiagonale(Tuile avTuile, Tuile tuile) {
+      return ((tuile.getC()== avTuile.getC()-1 && tuile.getL()==avTuile.getL()-1) || (tuile.getC()== avTuile.getC()+1 && tuile.getL()== avTuile.getL()-1) || (tuile.getL()+1== avTuile.getL()+1 && tuile.getC()== avTuile.getC()-1) || (tuile.getC()==avTuile.getC()+1 && tuile.getL()==avTuile.getL()+1));
+          
     }
     
+    private boolean tuileEstAdjacente(Tuile avTuile, Tuile tuile){
+        
+       return ((tuile.getC()==avTuile.getC() && tuile.getL()== avTuile.getL()-1) ||(tuile.getC()==avTuile.getC()+1 && tuile.getL()==avTuile.getL()) || (tuile.getC()==avTuile.getC() && tuile.getL()==avTuile.getL()+1) || (tuile.getC()==avTuile.getC()-1 && tuile.getL()==avTuile.getL()));
+    }
    
+    
+    public void Assecher(Aventurier av, Tuile tuile){
+        
+        if (tuile.getEtat()== EtatTuile.INONDEE && tuileEstAdjacente(av.getPositionCourante(), tuile)){
+             
+             tuile.setEtat(ASSECHEE);
+             
+             
+             
+        if (tuile.getEtat()== EtatTuile.INONDEE && tuileEstDiagonale(av.getPositionCourante(), tuile) && av.getCapacite()== Pion.VERT)     
+             
+             tuile.setEtat(ASSECHEE);
+         }
+         
+         
+    }
+    
+ 
     
 
     @Override
     public void update(Observable o, Object arg) {
     }
 
-    private boolean TuileEstDiagonale(Tuile avTuile, Tuile tuile) {
-      return ((tuile.getC()== avTuile.getC()-1 && tuile.getL()==avTuile.getL()-1) || (tuile.getC()== avTuile.getC()+1 && tuile.getL()== avTuile.getL()-1) || (tuile.getL()+1== avTuile.getL()+1 && tuile.getC()== avTuile.getC()-1) || (tuile.getC()==avTuile.getC()+1 && tuile.getL()==avTuile.getL()+1));
-          
-    }
+    
 
 }
