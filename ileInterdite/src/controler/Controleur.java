78410e920a7a0ed75e1c990091d7793988d0f364
morela1 <morela1@pc-dg-027-13>
2017-01-06 11:49:57 +0100

    Merge origin/master
    Conflicts:
    	ileInterdite/src/model/aventuriers/Aventurier.java
    	ileInterdite/src/model/cases/Tuile.java

--- ileInterdite/src/controler/Controleur.java
+++ ileInterdite/src/controler/Controleur.java
@@ -1,7 +1,13 @@
 package controler;
 
+
 import java.util.Observable;
 import java.util.Observer;
+import model.aventuriers.Aventurier;
+import model.cases.Tuile;
+import util.Utils;
+import util.Utils.EtatTuile;
+import util.Utils.Pion;
 
 /**
  *
@@ -11,16 +17,38 @@ public class Controleur implements Observer {
     //fezfzefzefz
     //tygifrio
     
-    
     public Controleur() {
+        
+        
     }
 
+    public void Assecher(Aventurier av, Tuile tuile){
+        Pion capacite;
+        capacite= av.getCapacite();
+        
+         if (tuile.getEtat()== EtatTuile.ASSECHEE && tuileAdjacente()){
+             
+             
+         }
+         
+        
+        
+         
+         
+        
+        
+    }
     
-    
+   
     
 
     @Override
     public void update(Observable o, Object arg) {
+    }
+
+    private boolean TuileEstDiagonale(Tuile avTuile, Tuile tuile) {
+      return ((tuile.getC()== avTuile.getC()-1 && tuile.getL()==avTuile.getL()-1) || (tuile.getC()== avTuile.getC()+1 && tuile.getL()== avTuile.getL()-1) || (tuile.getL()+1== avTuile.getL()+1 && tuile.getC()== avTuile.getC()-1) || (tuile.getC()==avTuile.getC()+1 && tuile.getL()==avTuile.getL()+1));
+          
     }
 
 }
