e37d49901d42bc6b76029296fe89b5732fff965c
asus <asus@pcasus.home>
2017-01-10 22:40:17 +0100

    Merge origin/master
    Conflicts:
    	ileInterdite/src/controler/Controleur.java

--- ileInterdite/src/view/VueInscription.java
+++ ileInterdite/src/view/VueInscription.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Observable;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -26,74 +27,82 @@ import util.Utils;
 public class VueInscription extends Observable {
 
     private JFrame window;
-    private JPanel panelGlobale, panelCentre, panelCentre1, panelCentre2, panelBas;
+    private JPanel panelGlobale,panelHaut, panelCentre, panelCentreTF, panelBas;
     private JButton bValider, bAnnuler;
-    private JLabel titre, textNbJ;
+    private JLabel titre, textNbJ, textNiv;
     private JComboBox nbJoueur;
     private JButton bOk;
     private JTextField joueur1, joueur2, joueur3, joueur4;
 
-    private Font font = new Font("Arial", 0, 30);
+    private Font font1 = new Font("Arial", 0, 25);
     private Font font2 = new Font("Arial", 0, 15);
     private GridLayout gl1 = new GridLayout(2, 2);
     private GridLayout gl2 = new GridLayout(1, 2);
 
     public VueInscription() {
         window = new JFrame();
-        window.setSize(300, 230);
+        window.setSize(340, 430);
         window.setAlwaysOnTop(true);//met en premier plan
         window.setUndecorated(true);//enlève le cadre de ta fenêtre
         Toolkit kit = Toolkit.getDefaultToolkit();
         Dimension dim = kit.getScreenSize();
-        window.setLocation(dim.width / 2 - 150, dim.height / 2 - 265);
+        window.setLocation(dim.width / 2 - 180, dim.height / 2 - 315);
         panelGlobale = new JPanel(new BorderLayout());
 
         //***haut***
-        titre = new JLabel("L'île Interdite");
-        titre.setFont(font);
+        titre = new JLabel(new ImageIcon(getClass().getResource("/images/titre.png")));
         panelGlobale.add(titre, BorderLayout.NORTH);
 
+        //***panel haut***
+        //texte "Choisir niveau"
+        panelHaut = new JPanel(new BorderLayout());
+        textNiv = new JLabel("Choisir niveau :");
+        textNiv.setFont(font1);
+        panelHaut.add(textNiv, BorderLayout.NORTH);
+        
+        
+        panelGlobale.add(panelHaut, BorderLayout.NORTH);
         //***panel centre***
         panelCentre = new JPanel();
-        //***panel centre 1 (haut du panel centre)***
-        panelCentre1 = new JPanel();
+        //***haut du panel centre***
         //texte "Rentrer les joueurs"
         textNbJ = new JLabel("Rentrer les joueurs :");
-        textNbJ.setFont(font2);
-
-        panelCentre1.add(textNbJ, BorderLayout.CENTER);
-
-        panelCentre.add(panelCentre1, BorderLayout.NORTH);
+        textNbJ.setFont(font1);
+        panelCentre.add(textNbJ, BorderLayout.NORTH);
         //***panel centre 2 (bas du panel centre)***
         gl1.setVgap(8);
         gl1.setHgap(8);
-        panelCentre2 = new JPanel(gl1);
+        panelCentreTF = new JPanel(gl1);
 
         //JTextField joueur 1
         joueur1 = new JTextField("Nom joueur");
-        joueur1.setPreferredSize(new Dimension(120, 28));
+        joueur1.setPreferredSize(new Dimension(130, 30));
+        joueur1.setFont(font2);
         insiterRentrerNom(joueur1);
-        panelCentre2.add(joueur1);
+        panelCentreTF.add(joueur1);
 
         //JTextField joueur 2
         joueur2 = new JTextField("Nom joueur");
-        joueur2.setPreferredSize(new Dimension(120, 28));
+        joueur2.setPreferredSize(new Dimension(130, 30));
+        joueur2.setFont(font2);
         insiterRentrerNom(joueur2);
-        panelCentre2.add(joueur2);
+        panelCentreTF.add(joueur2);
 
         //JTextField joueur 3
         joueur3 = new JTextField("Nom joueur");
-        joueur3.setPreferredSize(new Dimension(120, 28));
+        joueur3.setPreferredSize(new Dimension(130, 30));
+        joueur3.setFont(font2);
         insiterRentrerNom(joueur3);
-        panelCentre2.add(joueur3);
+        panelCentreTF.add(joueur3);
 
         //JTextField joueur 4
         joueur4 = new JTextField("Nom joueur");
-        joueur4.setPreferredSize(new Dimension(120, 28));
+        joueur4.setPreferredSize(new Dimension(130, 30));
+        joueur4.setFont(font2);
         insiterRentrerNom(joueur4);
-        panelCentre2.add(joueur4);
+        panelCentreTF.add(joueur4);
 
-        panelCentre.add(panelCentre2, BorderLayout.CENTER);
+        panelCentre.add(panelCentreTF, BorderLayout.CENTER);
 
         panelGlobale.add(panelCentre, BorderLayout.CENTER);
         //***panel Bas***
